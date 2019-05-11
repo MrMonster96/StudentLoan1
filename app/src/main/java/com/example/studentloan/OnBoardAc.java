30e550d865d39620daca75dad3524627c2a6f0d8
MrMonster96 <shubhamonster96@gmail.com>
2019-05-11 17:15:05 +0530

    add board  Activity

---
+++ app/src/main/java/com/example/studentloan/OnBoardAc.java
@@ -0,0 +1,52 @@
+package com.example.studentloan;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hololo.tutorial.library.Step;
+import com.hololo.tutorial.library.TutorialActivity;
+
+public class OnBoardAc extends TutorialActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String s1 = getString(R.string.app_name);
+        String s2 = getString(R.string.app_name);
+
+        //ONE SLIDE
+        addFragment(new Step.Builder().setTitle("How To Play")
+                .setContent(s1)
+                .setBackgroundColor(Color.parseColor("#598AC6")) // int background color
+                .setDrawable(R.drawable.ic_launcher_foreground) // int top drawable
+                .setSummary("Earn Daily using Refer your Friend")
+                .build());
+        // 2ND SLIDE
+        addFragment(new Step.Builder().setTitle("Rules And Regulation")
+                .setContent(s2)
+                .setBackgroundColor(Color.parseColor("#598AC6")) // int background color
+                .setDrawable(R.drawable.ic_launcher_background) // int top drawable
+                .setSummary("Only for Entertainment and help for society ")
+                .build());
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void currentFragmentPosition(int position) {
+
+    }
+    @Override
+    public void finishTutorial() {
+        // Your implementation
+        startActivity(new Intent(OnBoardAc.this,HomeActivity.class));
+        finish();
+    }
+}
